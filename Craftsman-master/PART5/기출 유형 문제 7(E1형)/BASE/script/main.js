bb7520b96ebffd0b26b92a4c9d58b5b0b492a49f
JeongGyul <jjascd@naver.com>
2024-11-03 16:00:13 +0900

    D3, E1, E2, E3 추가

---
+++ Craftsman-master/PART5/기출 유형 문제 7(E1형)/BASE/script/main.js
@@ -0,0 +1,35 @@
+let mainMenu = $('.main-menu > li');
+
+mainMenu.mouseover(function(){
+    $(this).find('ul').stop().slideDown();
+}).mouseout(function(){
+    $(this).find('ul').stop().slideUp();
+});
+
+let slideContainer = $('.slide-wrapper .container');
+let slide = slideContainer.find('.slide');
+let slideCount = slide.length;
+let currentIdx = 0;
+function autoSlide() {
+    setInterval(function(){
+        let nextIdx = (currentIdx + 1) % slideCount;
+        slideContainer.animate({
+            left: -100 * nextIdx + '%'
+        });
+        currentIdx = nextIdx;
+    }, 3000);
+}
+autoSlide();
+
+let popupLink = $('#notice li:first');
+let popup = $('#popup');
+let popupCloseBtn = popup.find('.close');
+
+popupLink.click(function(e){
+    e.preventDefault();
+    popup.addClass('active');
+});
+
+popupCloseBtn.click(function(){
+    popup.removeClass('active');
+});
